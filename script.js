23cb30cc3d9efaf42fd5b075aca3448a4beefa27
Mojaer Ahmed <mojaermohi@gmail.com>
2022-04-24 23:36:20 +0600

    cpmpleted javascreept

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 
 
-let locationBtn = document.getElementById('location-btn')
+const locationBtn = document.getElementById('location-btn')
 
-locationBtn.addEventListener('click',LonLat)
+locationBtn.addEventListener('click', LonLat)
 
 function LonLat() {
 
-  let Location=document.getElementById('location').value;
+  let Location = document.getElementById('location').value;
 
-  
+
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=b776eb19cb750275132cc185f36bf44b`)
     .then(resp => resp.json())
-    .then(data =>
+    .then(data => {
 
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=b776eb19cb750275132cc185f36bf44b`)
         .then(resp => resp.json())
-        .then(tempdata =>
+        .then(tempdata => {
+          const temperature = (tempdata.main.temp - 273.3);
 
-          console.log(tempdata.main.temp - 273.3))
+          console.log(tempdata)
+
+
+          document.getElementById('temp').innerText=temperature.toFixed(2);
+
+          document.getElementById('Name').innerText =tempdata.name;
+
+          document.getElementById('weatherd-discription').innerText = tempdata.weather[0].description
+        }
+
+
+        )
+    }
+
+
     )
 
-    console.log(Location)
-
-
 }
-
-
-
-LonLat()
